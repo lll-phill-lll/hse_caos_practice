999942a0695881d1a36d3da1a3cdd28d4f77a056
lll-phill-lll <filitovme@gmail.com>
2022-02-04 07:59:00 +0300

    added more comments

--- 10sem-fs/stat.c
+++ 10sem-fs/stat.c
@@ -1,3 +1,7 @@
+// принять через аргументы командной строки имя файла
+// Вывести его размер, номер iNode, права владельца и является ли
+// файл симлинком
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,21 +26,28 @@ int main(int argc, char **argv) {
 
     char perm[] = "---";
     printf("User permissions: ");
+    // https://www.opennet.ru/man.shtml?topic=fchmod&category=2&russian=0
+    // битовым И узнаем есть ли бит чтения для user
     if (file_stat.st_mode & S_IRUSR) {
         perm[0] = 'r';
     }
 
+    // битовым И узнаем есть ли бит записи для user
     if (file_stat.st_mode & S_IWUSR) {
         perm[1] = 'w';
     }
 
+    // битовым И узнаем есть ли бит исполнения для user
     if (file_stat.st_mode & S_IXUSR) {
+        // Если бит исполнения стоит и есть SUID,
+        // то выставляем s, иначе x
         if (file_stat.st_mode & S_ISUID) {
             perm[2] = 's';
         } else {
             perm[2] = 'x';
         }
     } else {
+        //Если SUID выставлен, но бита исполнения нет, то выставляем S
         if (file_stat.st_mode & S_ISUID) {
             perm[2] = 'S';
         }
