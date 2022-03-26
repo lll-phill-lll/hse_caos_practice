2636441c7e2f2e4f5e82430980efdac4e9132be8
Kristina Ostrovskaya <78148888+KrisOst-coder@users.noreply.github.com>
2022-03-26 13:01:33 +0300

    Update putenv.c

--- 13sem-fs/putenv.c
+++ 13sem-fs/putenv.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_env(const char* env_name) {
+void print_env(const char* env_name) {  // по прежнему печатает значение переменной окружения
     char* env = getenv(env_name);
     if (!env) {
         printf("not found\n");
@@ -11,15 +11,17 @@ void print_env(const char* env_name) {
 }
 
 int main() {
-
     print_env("HELLO");
-    putenv("HELLO=not world");
-    print_env("HELLO");
-
-    putenv("HELLO=");
+    
+    putenv("HELLO=not world");    // заменяет значение переменной окружения HELLO на not world
     print_env("HELLO");
 
-    putenv("HELLO");
+    
+    putenv("HELLO=");             // заменяет значение переменной окружения HELLO на \0
     print_env("HELLO");
 
+    
+    putenv("HELLO");              // указали переменную окружения без равно, поэтому переменная
+                                  // окружения удаляется в рамках нашей программы. и вывод - not found
+    print_env("HELLO");
 }
