265b5df904fb054e8dce1eee0a34bc1b01f805a5
Kristina Ostrovskaya <78148888+KrisOst-coder@users.noreply.github.com>
2022-03-26 12:25:55 +0300

    Update envp.c

--- 13sem-fs/envp.c
+++ 13sem-fs/envp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[], char* envp[]) {
-    while (*envp) {
+int main(int argc, char* argv[], char* envp[]) {   // в envp[] хранятся переменные окружения
+    while (*envp) {                                // выводятся все переменные окружения
         printf("%s\n", *envp++);
     }
 }
