77c1f916072b627182d36d1498e297e5dac563d2
Kristina Ostrovskaya <78148888+KrisOst-coder@users.noreply.github.com>
2022-03-26 17:33:16 +0300

    Update exec_fork.c

--- 13sem-fs/exec_fork.c
+++ 13sem-fs/exec_fork.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+// fork + exec
 int main() {
-
-    pid_t pid = fork();
+    pid_t pid = fork(); // создаем ребенка
     if (pid < 0) {
         perror("fork");
         exit(1);
-    } else if (!pid) {
-        execl("/usr/bin/echo", "echo", "hello world", NULL);
+    } else if (!pid) {  // тело ребенка заменяем другим процессом, который хотим выполнить
+        execl("/usr/bin/echo", "echo", "hello world", NULL);   // выведет hello world
         // execl("/usr/bin/sleep", "echo", "100", NULL);
         perror("exec");
         _exit(1);
     }
-
+    
+    // в родители продолжаем свои делишки
     int status;
-    if (waitpid(pid, &status, 0) == -1) {
+    if (waitpid(pid, &status, 0) == -1) {  // ждем выполнения ребенка
         perror("waitpid");
         exit(1);
     }
-
-    if (WIFEXITED(status)) {
-        printf("Exit status: %d\n", WEXITSTATUS(status));
+    
+    if (WIFEXITED(status)) {  // смотрим на статус ребенка
+        printf("Exit status: %d\n", WEXITSTATUS(status));     // status = 0
     }
-
     return 0;
 }
