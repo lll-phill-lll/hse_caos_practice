ab26ef80905e6b477eb54ef3ab7b51d5599770ed
MSF <80293552+mengsifei@users.noreply.github.com>
2022-04-14 02:16:33 +0300

    Update strtime.c

--- 17sem-fs/strtime.c
+++ 17sem-fs/strtime.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    char outstr[200];
+    char outstr[200]; 
     time_t t;
     struct tm *tmp;
 
@@ -15,8 +15,17 @@ int main(int argc, char *argv[]) {
        perror("localtime");
        exit(EXIT_FAILURE);
     }
-
+    
+    // подробнее: man strftime
+    // strftime представляет структуру tm в виде отформатированной строки
     if (strftime(outstr, sizeof(outstr), argv[1], tmp) == 0) {
+        // Возвращает количество символов, записанных в массив,если в нем достаточно места
+        // в противном случае возвращает 0
+        // В данном случае:
+        // outstr - куда нужно положить результат
+        // sizeof(outstr) - размер
+        // argv[1] - формат
+        // tmp - указатель на структуру tm
        fprintf(stderr, "strftime returned 0");
        exit(EXIT_FAILURE);
     }
