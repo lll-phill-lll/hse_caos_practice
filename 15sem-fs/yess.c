a15df005499142c429be0fa55f1434d5f0bf90f9
Kristina Ostrovskaya <78148888+KrisOst-coder@users.noreply.github.com>
2022-04-14 12:57:24 +0300

    Update yess.c

--- 15sem-fs/yess.c
+++ 15sem-fs/yess.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+// принимает кол-во секунд, которое программа будет спать и строчку, которую будет печаттаь
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         fprintf(stderr, "Usage: %s DELAY STRING\n", argv[0]);
@@ -12,10 +13,9 @@ int main(int argc, char *argv[]) {
     int delay = atoi(argv[1]);
     int counter = 1;
 
+    //спит печатает, спит печатает и за одно выводит id, который иногда нужен, чтобы прерывать программу определенными сигналами
     while(1) {
         sleep(delay);
         printf("[%d] %d: %s\n", getpid(), counter++, argv[2]);
     }
-
 }
-
