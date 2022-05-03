21272ce47f73e48024ca6dbcd66b0cf546bf3275
Frosendroska <katyabraun0@gmail.com>
2022-05-03 17:44:10 +0300

    comments for 5

--- 5sem-c/scanf_example.c
+++ 5sem-c/scanf_example.c
@@ -6,16 +6,16 @@ int main() {
     unsigned u;
     char s[64];
 
-    scanf("%c", &c);
+    scanf("%c", &c); // char
     printf("%c\n", c);
 
-    scanf("%d", &i);
+    scanf("%d", &i); // digit
     printf("%d\n", i);
 
-    scanf("%u", &u);
+    scanf("%u", &u); // unsigned
     printf("%u\n", u);
 
-    scanf("%s", s);
+    scanf("%s", s); // string
     printf("%s\n", s);
 }
 
--- 5sem-c/speed_comp.c
+++ 5sem-c/speed_comp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Отключено выравнивание и размер ее будет 10
 struct S_p {
     unsigned char c1;
     unsigned int i1;
@@ -7,6 +8,7 @@ struct S_p {
     unsigned int i2;
 }__attribute__((packed));
 
+// Есть выравнивание и она занимает 16
 struct S_a {
     unsigned char c1;
     unsigned int i1;
@@ -20,11 +22,13 @@ int main() {
     printf("%ld %ld\n", sizeof(struct S_p), sizeof(struct S_a));
     for (unsigned i = 0; i != 100000000; ++i) {
         for (unsigned j = 0; j != 10; ++j) {
+            // прибавляем единички к полям много раз
             s.c1 += 1;
             s.i1 += 1;
             s.c2 += 1;
             s.i2 += 1;
         }
     }
-
+ // На некоторых архитектурах разницы во времени почти нет 2.007 и 2.032, 
+ // но на каких-то может быть кардинально быстрее
 }
