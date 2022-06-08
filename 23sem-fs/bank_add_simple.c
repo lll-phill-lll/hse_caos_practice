28caa6d9853d93aa408dc6b9b54f45bc563fa321
Kristina Ostrovskaya <78148888+KrisOst-coder@users.noreply.github.com>
2022-06-08 09:25:31 +0300

    Update bank_add_simple.c

--- 23sem-fs/bank_add_simple.c
+++ 23sem-fs/bank_add_simple.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
+// структура, где хранится текущий баланс
 typedef struct {
     int balance;
 } Account;
 
+// печатает баланс
 void print_account(Account account) {
     printf("%d\n", account.balance);
 }
 
+// добавляем денюжку
 void add_balance(Account* account, int amount) {
     account->balance += amount;
 }
 
+// 1000 раз добавляем по одной монетке
 void process(Account* account) {
     for (int i = 0; i != 1000; ++i) {
         add_balance(account, 1);
@@ -28,5 +31,7 @@ int main() {
     process(&account);
 
     print_account(account);
-
 }
+
+// вроде бы все хорошо, но нужно предусмотреть, что у нас будет 100500 пользователей и еще больше операций с балансом
+// и вспомним, что мы знаем про thread-ы
