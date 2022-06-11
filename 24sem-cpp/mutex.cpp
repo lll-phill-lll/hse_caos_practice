2e4c114d5e8564b9f6d819ae69e6a020c038a31f
MSF <80293552+mengsifei@users.noreply.github.com>
2022-06-11 22:21:31 +0300

    Update mutex.cpp

--- 24sem-cpp/mutex.cpp
+++ 24sem-cpp/mutex.cpp
@@ -9,6 +9,12 @@ void func(int n) {
     for (int i = 0; i != n; ++i) {
         // m.lock();
         std::lock_guard<std::mutex> lg(m);
+        /*
+        Больше про lock_guard можно найти здесь: https://en.cppreference.com/w/cpp/thread/lock_guard
+        std::lock_guard эквивалентна mutex.lock() + mutex.unlock(). Эта функция безопаснее, так как при унитожении
+        объекта lock_guard автоматически вызывается unlock, и не будет deadlock, если случится исключение до 
+        момента unlock.  
+        */
         var += i;
         // m.unlock()
     }
