bb85de91a340f6bcbe905c599b31b47fbbfe9a6b
MSF <80293552+mengsifei@users.noreply.github.com>
2022-06-11 22:34:46 +0300

    Update futures.cpp

--- 24sem-cpp/futures.cpp
+++ 24sem-cpp/futures.cpp
@@ -2,17 +2,23 @@
 #include <thread>
 #include <future>
 
+/* Как получить значения, которые возвращаются из функции?
+Для этого создаем promise и future. Они работают аналогично каналу. 
+В promise будем писать, из future - забирать
+*/
+
 void func(std::promise<int> p) {
-    p.set_value(123);
+    p.set_value(123);  // пишем 123 в promise
 }
 
 int main() {
     std::promise<int> p;
     std::future<int> f = p.get_future();
 
-    std::thread thr(func, std::move(p));
+    std::thread thr(func, std::move(p)); // в thread передаем promise, чтобы thread знал, куда писать
+    // promise нельзя скопировать, поэтому используем move.
 
-    int res = f.get();
+    int res = f.get(); // забираем значение от future
 
     thr.join();
 
@@ -20,5 +26,6 @@ int main() {
     std::cout << res << std::endl;
 
     // exception
-    // res = f.get();
+    // res = f.get(); механизм работает только 1 раз.
+    // Если 2 раза забираем значение от future, будет ошибка: no associated state
 }
