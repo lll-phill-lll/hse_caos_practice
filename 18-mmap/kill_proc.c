2cba4f185a162032bd139af0dfde91c04ed2d1fb
Filitov Mikhail <filitovme@gmail.com>
2024-03-24 17:28:35 +0300

    added 18 class

---
+++ 18-mmap/kill_proc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Открыть файл и записать его в виртуальную память.
+ * Заменить каждый символ на 'c' в этом файле
+ * файл для теста - small_file.txt
+ */
+
+// printf
+#include <stdio.h>
+// exit
+#include <stdlib.h>
+// mmap, munmap
+#include <sys/mman.h>
+// fstat
+#include <sys/stat.h>
+// open
+#include <fcntl.h>
+// close
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    // Выделяем 2 гб памяти
+    size_t size = 2ULL * 1024 * 1024 * 1024;
+
+    // Видим, что физической памяти выделилось сильно меньше, чем виртуальной
+    // cat /proc/<pid>/status - VmSize - сколько выделилось виртуальной памяти,
+    // VmRss - сколько физической
+    //
+    void* ptr = malloc(size);
+
+    // Просто пауза, которую мы можем прервать введя символ
+    getchar();
+
+    // есть запустим с аргументом командной строки, то тогда память
+    // будет заполняться единицами и ее придется физически выделять.
+    // Можно в htop посмотреть на свободную память и размер swap файла
+    if (argc > 1) {
+        memset(ptr, '1', size);
+        printf("success\n");
+        getchar();
+    }
+}
