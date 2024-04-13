c15b7537ba702c4b1ce7abbc87e488c7bb34b83b
Filitov Mikhail <filitovme@gmail.com>
2024-04-13 14:08:45 +0200

    Done

--- 23-builds/main.cpp
+++ 23-builds/main.cpp
@@ -4,9 +4,24 @@
 #include "grayscale.h"
 #include "blur.h"
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Choose: Gray or Blur" << std::endl;
+        exit(1);
+    }
+
     PPMImage img;
     img.ReadFromFile("dogs.ppm");
-    ApplyBlur(img.data, img.x, img.y);
-    img.SaveToFile("dogsBlurred.ppm");
+
+    std::string input = argv[1];
+
+    if (input == "Gray") {
+        MakeGray(img.data);
+    } else if (input == "Blur") {
+        ApplyBlur(img.data, img.x, img.y);
+    } else {
+        std::cerr << "Not recognized" << std::endl;
+        exit(1);
+    }
+    img.SaveToFile("dogsResult.ppm");
 }
