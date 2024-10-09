98d3462b6f5607b05d025f53dc53ef95a1a62e63
lll-phill-lll <filitovme@gmail.com>
2024-10-09 11:08:18 +0000

    added 4 class

---
+++ 04-cdecl/lea.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n * 8);
+
+    printf("%d\n", n * 3);
+    printf("%d\n", n * 7);
+    printf("%d\n", n * 9);
+    printf("%d\n", n * 12);
+    printf("%d\n", n * 15);
+
+    printf("%d\n", n * 8342);
+
+
+
+}
